0a7386d4b8d7a277694e3e9465351a208700db25
davejmacm <davejmacm@gmail.com>
2019-03-25 22:55:57 +0000

    Adding auth to app; created login route, set that to initial path (bypasses main) - no logic implimented yet. Moved images folder; npm start threw error about image location so moves images folder from public to src. Added firebase.json file to localhost it - nothing displaying just now.

--- fantasy_got/src/App.js
+++ fantasy_got/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './Components/Main.js';
+import Login from './Components/Login.js';
 import * as firebase from 'firebase';
 
 class App extends Component {
   render() {
     return (
       <div className="home-page">
-        <Main/>
+        
+        <Login/>
       </div>
     );
   }
---
+++ fantasy_got/src/Components/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => (
+  <div className="login-container">
+    <div class="login-fields">
+      <input id="fieldEmail" type="email" placeholder="Email"/>
+
+      <input id="fieldPassword" type="password" placeholder="Password"/>
+
+      <button id="btnLogin" className="btn-Login">Log in</button>
+
+      <button id="btnSignup" className="btn-Signup">Sign up</button>
+      
+      </div>
+  </div>
+);
+
+export default Login;
