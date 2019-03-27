f5acbd6e1a5f56ceda341eadaa867aa72750e472
davejmacm <davejmacm@gmail.com>
2019-03-27 22:36:40 +0000

    Added logout button - functionality working, no css or placement

--- fantasy_got/src/Components/Main.js
+++ fantasy_got/src/Components/Main.js
@@ -3,21 +3,27 @@ import "../App.css";
 import Navbar from "./Navbar";
 import Scoring from "./Scoring";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import fire from '../config/fire'
 
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.logout = this.logout.bind(this)
     this.state ={
       status: "homePage"
     }
   };
 
+logout(){
+  fire.auth().signOut();
+}
 
 render(){
   return (
     <Router>
       <React.Fragment>
         <Navbar />
+        <button onClick={this.logout}>Logout</button>
         <Route path="/scoring" component={Scoring} />
       </React.Fragment>
     </Router>
