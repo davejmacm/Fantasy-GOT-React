6110b18a489e935fc5d0427212865b942a174d69
davejmacm <davejmacm@gmail.com>
2019-03-27 20:51:52 +0000

    Starting implimentation of login flow; moved firebase initialization to seperate file

---
+++ fantasy_got/src/config/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBQBvvRD0fRGLIwG2eF145pcXcDeq0yFxg",
+  authDomain: "fantasy-got.firebaseapp.com",
+  databaseURL: "https://fantasy-got.firebaseio.com",
+  projectId: "fantasy-got",
+  storageBucket: "fantasy-got.appspot.com",
+  messagingSenderId: "837780020674"
+};
+const fire = firebase.initializeApp(config);
+export default fire;
