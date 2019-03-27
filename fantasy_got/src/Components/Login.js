f6322a029a66544e0d6f6f500b3dc682f050a5aa
davejmacm <davejmacm@gmail.com>
2019-03-27 23:15:58 +0000

    Added sign up functionality - working; sorted background image responsiveness

--- fantasy_got/src/Components/Login.js
+++ fantasy_got/src/Components/Login.js
@@ -6,6 +6,7 @@ class Login extends Component {
     super(props);
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
     this.state = {
       email: '',
       password: ''
@@ -17,6 +18,14 @@ class Login extends Component {
     e.preventDefault();
     fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
     }).catch((error)=> {
+      console.log(error);
+      alert("User name or password not recognised - please try again or 'Sign up'." )//TODO add custom alert/popup box https://www.google.com/search?safe=strict&rlz=1C1ASUM_enGB534GB534&ei=_fubXO3SL-yr1fAP_ZC1wAo&q=how+to+make+a+nicer+alert+in+javascript&oq=how+to+make+a+nicer+alert+&gs_l=psy-ab.1.1.33i22i29i30l10.6089.10708..12676...0.0..0.128.2094.22j4....2..0....1..gws-wiz.......0i71j35i39j0i273j0i131j0j0i10i67j0i67j0i22i30.KA2FCR80s3s#kpvalbx=1
+    });
+  }
+
+  signup(e){
+    e.preventDefault();
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch((error)=> {
       console.log(error);
     });
   }
