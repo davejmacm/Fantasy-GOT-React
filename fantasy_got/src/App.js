1b66730cf4ee88461e8fe1a4bdc1030295cfc541
davejmacm <davejmacm@gmail.com>
2019-03-27 22:01:39 +0000

    Added login page flow to take user straight to login page if not logged in, and home if they are BUT have issue with text fields

--- fantasy_got/src/App.js
+++ fantasy_got/src/App.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './Components/Main.js';
 import Login from './Components/Login.js';
-import * as firebase from 'firebase';
+import fire from './config/fire';
+// import * as firebase from 'firebase';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.authListener = this.authListener.bind(this);
+    this.state = {
+      user: {}
+    }
+}
+
+componentDidMount(){
+  this.authListener();
+}
+
+  //Add realtime listener to check for logged in user
+  authListener(){
+  fire.auth().onAuthStateChanged((user) => {
+    if(user) {
+      this.setState({ user });
+      console.log(user);
+    } else {
+      this.setState({ user: null });
+      console.log('not logged in',this.setState.user);
+    }
+  });
+}
+
+
   render() {
     return (
       <div className="home-page">
-        
-        <Login/>
+      {this.state.user ? (<Main />) : (<Login/>)}
+
       </div>
     );
   }
--- fantasy_got/src/Components/Login.js
+++ fantasy_got/src/Components/Login.js
@@ -1,19 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import fire from '../config/fire';
 
-const Login = () => (
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+  }
+
+  login(e) {
+    e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).catch((error)=> {
+      console.log(error);
+    });
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+  render() {
+    return(
+
   <div className="login-container">
+   <h1> Log in to join the fight!</h1>
+    <form>
     <div className="login-fields">
-      <input id="fieldEmail" type="email" placeholder="Email"/>
+      <input id="fieldEmail" value={this.state.email} onChange={this.handleChange} type="email" placeholder="Enter Email"/>
 
-      <input id="fieldPassword" type="password" placeholder="Password"/>
+      <input id="fieldPassword" value={this.state.password} onChange={this.handleChange} type="password" placeholder="Password"/>
 
-      <button id="btnLogin" className="btn-Login">Log in</button>
+      <button id="btnLogin" className="btn-Login" type = "submit" onClick={this.Login}>Log in</button>
 
-      <button id="btnSignup" className="btn-Signup">Sign up</button>
+      <button id="btnSignup" className="btn-Signup" onClick={this.signup}>Sign up</button>
 
       </div>
+    </form>
   </div>
-);
+   )
+  }
+};
 
 export default Login;
